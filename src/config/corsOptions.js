2b61a1da6c37c760f8b1df891317d95c6d659a77
jakub41 <lemiszewski@gmx.com>
2019-12-17 16:41:08 +0100

    Fixed whitelist  heroku complain

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,8 +1,8 @@
-const { whitelist } = require("./whiteList")
+const whiteList = ["http://localhost:3000", process.env.FE_URL];
 
 const corsOptions = {
     origin: function(origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
+        if (whiteList.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
             callback(new Error("Not allowed by CORS"));
